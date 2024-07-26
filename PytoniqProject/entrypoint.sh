70ea50669c6e8d046e3f6dde46c5fde7608719ce
*cr0x6fw <115905615+d0x65viant@users.noreply.github.com>
2024-07-26 15:38:12 +0300

    modified: PytoniqProject/entrypoint.sh

--- PytoniqProject/entrypoint.sh
+++ PytoniqProject/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Запуск Celery в фоновом режиме
+celery -A worker.celery_app worker --loglevel=info &
+
+# Запуск Flower в фоновом режиме
+celery -A worker.celery_app flower &
+
+# Дождаться, пока Celery и Flower запустятся, можно использовать sleep или проверку состояния.
 # Запуск основной программы
-celery -A worker.celery_app worker --loglevel=info
-celery -A worker.celery_app flower
 python __main__.py
 
 # Удержание контейнера активным
 exec sleep infinity
-
